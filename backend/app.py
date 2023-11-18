f71da2fc8ee9821f9b18c8342fe48fe4ce3d10bb
Vivi <nguyen.chaudiem@yahoo.de>
2023-11-18 11:55:13 +0100

    Chains but with max. 200 tokens

--- backend/app.py
+++ backend/app.py
@@ -9,13 +9,10 @@ import openai
 # Create Flask app
 # app = Flask(__name__)
 
-# # Prompt templates
-# class ProductionStepsPromptTemplate(PromptTemplate):
-#     def generate_prompt(self, data):
-#         return {
-#             "prompt": f"Wie wäre ein Werk aufgebaut, welches {data['product']} produziert? Bitte gib eine Liste ohne zusätzliche Erklärungen aus.",
-#             "max_tokens": 150
-#         }
+# Read background information from txt file
+def read_background_info(file_path):
+    with open(file_path, 'r', encoding='utf-8') as file:
+        return file.read()
 
 def main():
     # Load environment variables
@@ -74,20 +71,26 @@ def main():
 
     # 3. Chain Skills
 
+    # Load background information
+    background_info_path = "backend/documents/background_info.txt" # Adjust the path as needed
+    background_info = read_background_info(background_info_path)
+
     # Create prompt template
-    skills_template_str = "Angenommen in der Produktion von dem Produkt {product} gibt es folgende folgende Rollen {roles}. Bitte gib eine Liste mit relevanten Fähigkeiten und ohne zusätzliche Erklärungen aus, die jede dieser Rollen benötigt."
-    formatted_skills_template = skills_template_str.format(product=product, roles=roles_response)
+    skills_template_str = "Angenommen in der Produktion von dem Produkt {product} gibt es folgende Rollen: {roles}. Außerdem gibt es folgende Kompetenzkategorien: {background_info}. Bitte gib eine Liste mit relevanten Fähigkeiten und ohne zusätzliche Erklärungen aus, die jede dieser Rollen benötigt, und kategorisiere diese Fähigkeiten in die vier Kompetenzkategorien."
+    formatted_roles_template = skills_template_str.format(product=product, roles=roles_response, background_info=background_info)
 
     # Create prompt
     prompt_skills = PromptTemplate(
-        input_variables=["product", "roles"], template=formatted_skills_template
+        input_variables=["product", "roles", "background_info"], 
+        template=formatted_roles_template,
+        max_tokens=900
     )
 
     # Chain for skills
     skills_chain = LLMChain(llm=llm, prompt=prompt_skills)
 
     # Define input variables for the chain
-    input_variables = {"product": product, "roles": roles_response}
+    input_variables = {"product": product, "roles": roles_response, "background_info": background_info}
 
     # Get skills from OpenAI
     skills_response = skills_chain.run(input_variables)
