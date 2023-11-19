5b35c311d0e088235278e6ad849cb69bba599711
Vivi <nguyen.chaudiem@yahoo.de>
2023-11-19 15:00:52 +0100

    First steps for sequential chain

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,7 @@
 # from flask import Flask
 from langchain.prompts import PromptTemplate
 from langchain.chat_models import ChatOpenAI
+from langchain.schema import StrOutputParser
 from langchain.chains import LLMChain
 from pathlib import Path
 import dotenv
@@ -32,84 +33,106 @@ def main():
     )   
 
     # Initial user input
+    # product = input("Welches Produkt wird in dem Werk produziert? ")
+
+    # Prompt templates
+    # production_steps_template_str = "Welche Fertigungslinien bzw. Arbeitsschritte könnten in der Produktion von dem Produkt {product} vorkommen? Bitte gib eine Liste ohne zusätzliche Erklärungen aus."
+    # formatted_production_steps_template = production_steps_template_str.format(product=product)
+
+    # roles_template_str = "Angenommen in der Produktion von dem Produkt {product} gibt es folgende Arbeitsschritte {production_steps}. Welche Rollen könnte es jede dieser Arbeitsschritte geben? Bitte gib eine Liste ohne zusätzliche Erklärungen aus."
+    # formatted_roles_template = roles_template_str.format(product=product, production_steps=production_steps_response)
+
+    production_steps_prompt = PromptTemplate.from_template(
+        "Welche Fertigungslinien bzw. Arbeitsschritte könnten in der Produktion von dem Produkt {product} vorkommen? Bitte gib eine Liste ohne zusätzliche Erklärungen aus."
+    )
+
+    roles_template_prompt = PromptTemplate.from_template(
+        "Angenommen es gibt folgende Arbeitsschritte {production_steps} in einer Produktion. Welche Rollen könnte es jede dieser Arbeitsschritte geben? Bitte gib eine Liste ohne zusätzliche Erklärungen aus."
+    )
+
+    chain = (
+        {"production_steps": production_steps_prompt | llm | StrOutputParser()}
+        | roles_template_prompt
+        | llm
+        | StrOutputParser()
+    )
+
     product = input("Welches Produkt wird in dem Werk produziert? ")
+
+    output = chain.invoke({"product": product})
+    print(output)
 
     # 1. Chain: Production steps
 
-    # Create prompt template
-    production_steps_template_str = "Welche Fertigungslinien bzw. Arbeitsschritte könnten in der Produktion von dem Produkt {product} vorkommen? Bitte gib eine Liste ohne zusätzliche Erklärungen aus."
-    formatted_production_steps_template = production_steps_template_str.format(product=product)
+    # Create prompt
+    # prompt_production_steps = PromptTemplate(
+    #     input_variables=["product"], template=formatted_production_steps_template
+    # )
+
+    # # Chain for production steps
+    # production_steps_chain = LLMChain(llm=llm, prompt=prompt_production_steps)
+
+    # # Define input variables for the chain
+    # input_variables = {"product": product}
+
+    # # Get production steps from OpenAI
+    # production_steps_response = production_steps_chain.run(input_variables)
+    # print("\nArbeitsschritte:")
+    # print(production_steps_response)
+
+    # # 2. Chain: Roles
+
+    # # Create prompt template
     
-    # Create prompt
-    prompt_production_steps = PromptTemplate(
-        input_variables=["product"], template=formatted_production_steps_template
-    )
 
-    # Chain for production steps
-    production_steps_chain = LLMChain(llm=llm, prompt=prompt_production_steps)
+    # # Create prompt
+    # prompt_roles = PromptTemplate(
+    #     input_variables=["product", "production_steps"], template=formatted_roles_template
+    # )
 
-    # Define input variables for the chain
-    input_variables = {"product": product}
+    # # Chain for roles
+    # roles_chain = LLMChain(llm=llm, prompt=prompt_roles)
 
-    # Get production steps from OpenAI
-    production_steps_response = production_steps_chain.run(input_variables)
-    print("\nArbeitsschritte:")
-    print(production_steps_response)
+    # # Define input variables for the chain
+    # input_variables = {"product": product, "production_steps": production_steps_response}
 
-    # 2. Chain: Roles
-
-    # Create prompt template
-    roles_template_str = "Angenommen in der Produktion von dem Produkt {product} gibt es folgende Arbeitsschritte {production_steps}. Welche Rollen könnte es jede dieser Arbeitsschritte geben? Bitte gib eine Liste ohne zusätzliche Erklärungen aus."
-    formatted_roles_template = roles_template_str.format(product=product, production_steps=production_steps_response)
-
-    # Create prompt
-    prompt_roles = PromptTemplate(
-        input_variables=["product", "production_steps"], template=formatted_roles_template
-    )
-
-    # Chain for roles
-    roles_chain = LLMChain(llm=llm, prompt=prompt_roles)
-
-    # Define input variables for the chain
-    input_variables = {"product": product, "production_steps": production_steps_response}
-
-    # Get roles from OpenAI
-    roles_response = roles_chain.run(input_variables)
-    print("\nRollen:")
-    print(roles_response)
+    # # Get roles from OpenAI
+    # roles_response = roles_chain.run(input_variables)
+    # print("\nRollen:")
+    # print(roles_response)
 
     # 3. Chain Skills
 
-    # Load background information
-    background_info_path = "backend/documents/background_info.txt" 
-    background_info = read_background_info(background_info_path)
+    # # Load background information
+    # background_info_path = "backend/documents/background_info.txt" 
+    # background_info = read_background_info(background_info_path)
 
-    # Load the JSON-style template as a raw string
-    skills_json_template_path = "backend/documents/skills_json_description.txt"
-    with open(skills_json_template_path, 'r', encoding='utf-8') as file:
-        skills_json_template_raw = file.read()
+    # # Load the JSON-style template as a raw string
+    # skills_json_template_path = "backend/documents/skills_json_description.txt"
+    # with open(skills_json_template_path, 'r', encoding='utf-8') as file:
+    #     skills_json_template_raw = file.read()
 
-    # Construct the prompt using string concatenation
-    skills_template_str = (
-        "Angenommen in der Produktion von dem Produkt {product} gibt es folgende Rollen: {roles} und die zu folgenden Arbeitsschritten gehören: {production_steps}. Außerdem gibt es folgende Kompetenzkategorien mit folgenden Definitionen: {background_info}. Bitte gib mir für die erste Rolle eine Liste an Fähigkeiten aus, die in den vorgegeben Kompetenzkategorien eingeordnet sind. Bitte strukturiere die Antwort gemäß der folgenden JSON-Vorlage für jede einzigartige Rolle:\n" + skills_json_template_raw
-    )
+    # # Construct the prompt using string concatenation
+    # skills_template_str = (
+    #     "Angenommen in der Produktion von dem Produkt {product} gibt es folgende Rollen: {roles} und die zu folgenden Arbeitsschritten gehören: {production_steps}. Außerdem gibt es folgende Kompetenzkategorien mit folgenden Definitionen: {background_info}. Bitte gib mir für die erste Rolle eine Liste an Fähigkeiten aus, die in den vorgegeben Kompetenzkategorien eingeordnet sind. Bitte strukturiere die Antwort gemäß der folgenden JSON-Vorlage für jede einzigartige Rolle:\n" + skills_json_template_raw
+    # )
 
-    # Create the prompt without using .format()
-    prompt_skills = PromptTemplate(
-        input_variables=["product", "roles", "production_steps", "background_info"], 
-        template=skills_template_str
-    )
+    # # Create the prompt without using .format()
+    # prompt_skills = PromptTemplate(
+    #     input_variables=["product", "roles", "production_steps", "background_info"], 
+    #     template=skills_template_str
+    # )
 
-    # Chain for skills
-    skills_chain = LLMChain(llm=llm, prompt=prompt_skills)
+    # # Chain for skills
+    # skills_chain = LLMChain(llm=llm, prompt=prompt_skills)
 
-    # Define input variables for the chain
-    input_variables = {"product": product, "roles": roles_response, "production_steps": production_steps_response, "background_info": background_info}
+    # # Define input variables for the chain
+    # input_variables = {"product": product, "roles": roles_response, "production_steps": production_steps_response, "background_info": background_info}
 
-    # Get skills from OpenAI
-    skills_response = skills_chain.run(input_variables)
-    print("\nFähigkeiten:")
-    print(skills_response)
+    # # Get skills from OpenAI
+    # skills_response = skills_chain.run(input_variables)
+    # print("\nFähigkeiten:")
+    # print(skills_response)
 
 if __name__ == '__main__':
    main()
