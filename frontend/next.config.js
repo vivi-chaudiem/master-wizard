cb11d9b33ff6a7f501eb5920bc8c9c8fb6d5f763
Vivi <nguyen.chaudiem@yahoo.de>
2024-01-24 18:32:39 +0700

    Added basic authentication

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,3 +2,14 @@
 const nextConfig = {}
 
 module.exports = nextConfig
+
+const withBasicAuth = require('next-basic-auth');
+
+module.exports = withBasicAuth({
+  basicAuth: {
+    users: {
+      [process.env.BASIC_AUTH_USERNAME]: process.env.BASIC_AUTH_PASSWORD,
+    },
+    unauthorizedResponse: 'Unauthorized',
+  }
+});
