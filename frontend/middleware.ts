b4080e79ae98bc450b667d3b380cd7628cd41e62
Vivi <nguyen.chaudiem@yahoo.de>
2024-01-25 09:55:34 +0700

    Added middleware.ts

---
+++ frontend/middleware.ts
@@ -0,0 +1,29 @@
+import { NextRequest, NextResponse } from 'next/server';
+
+export const config = {
+  matcher: '*', // Apply the middleware to all routes
+};
+
+export function middleware(req: NextRequest) {
+  const basicAuth = req.headers.get('authorization');
+
+  if (basicAuth) {
+    const authValue = basicAuth.split(' ')[1];
+    const [user, pwd] = atob(authValue).split(':');
+
+    const validUser = process.env.BASIC_AUTH_USERNAME;
+    const validPwd = process.env.BASIC_AUTH_PASSWORD;
+
+    if (user === validUser && pwd === validPwd) {
+      return NextResponse.next(); // Continue with the request if authentication is valid
+    }
+  }
+
+  // Return a custom error message when authentication fails
+  return new Response('Falsche Logindaten', {
+    status: 401, // HTTP status code for Unauthorized
+    headers: {
+      'Content-Type': 'text/plain', // Set the content type to plain text
+    },
+  });
+}
--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,15 +1,17 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
-
 const withBasicAuth = require('next-basic-auth');
 
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
+
 module.exports = withBasicAuth({
+  ...nextConfig,
+
+  // Basic Auth configuration
   basicAuth: {
     users: {
       [process.env.BASIC_AUTH_USERNAME]: process.env.BASIC_AUTH_PASSWORD,
     },
+    challenge: true,
     unauthorizedResponse: 'Unauthorized',
-  }
+  },
 });
